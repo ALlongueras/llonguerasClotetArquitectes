6d3018583a53bf44a9ace353b83def1e31faffd1
allongueras <albert@albertllongueras.cat>
2012-11-17 14:37:30 +0100

    loading publicacions

--- llonguerasclotet/scripts/publicacions.js
+++ llonguerasclotet/scripts/publicacions.js
@@ -1,5 +1,6 @@
 ﻿$(window).load(function () {
     $('#publicacions .projecte').isotope({ filter: ".publicacionsPremis" });
+    $("#loader").addClass("hide");
     $('#publicacions .projecte div').each(function () {
         $(this).removeClass("hide");
     });
@@ -9,48 +10,13 @@
         return false;
     });
     $(".publicacionsPremis a").click(function () {
-//        $(".publicacionsPremis").hasClass(".publicacions-fancybox").removeClass("publicacions-fancybox");
         $(this).addClass("publicacions-fancybox");
         $(".publicacions-fancybox").fancybox({
-        helpers: {
-            title: {
-                type: 'outside'
+            helpers: {
+                title: {
+                    type: 'outside'
+                }
             }
-        }
+        });
     });
-    });
-});
-
-$(document).ready(function () {
-    
-    
-
-    //    $("#single_2").fancybox({
-    //        openEffect: 'elastic',
-    //        closeEffect: 'elastic',
-
-    //        helpers: {
-    //            title: {
-    //                type: 'inside'
-    //            }
-    //        }
-    //    });
-
-    //    $("#single_3").fancybox({
-    //        openEffect: 'none',
-    //        closeEffect: 'none',
-    //        helpers: {
-    //            title: {
-    //                type: 'outside'
-    //            }
-    //        }
-    //    });
-
-    //    $("#single_4").fancybox({
-    //        helpers: {
-    //            title: {
-    //                type: 'over'
-    //            }
-    //        }
-    //    });
 });
