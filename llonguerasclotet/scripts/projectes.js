1e342a169b158e6c77626ecd18bbea4d46fe9c47
allongueras <albert.llongueras@newshore.es>
2013-10-20 21:07:45 +0200

    Main menu changes and project images

--- llonguerasclotet/scripts/projectes.js
+++ llonguerasclotet/scripts/projectes.js
@@ -1,30 +1,43 @@
 ﻿$(function() {
     actualitzarAttr("height", $('#valorFilesImatges').val(), $('#idFilesImatges').val());
     var imatgePetitaAnterior = $('#imatgesProjectes .contenidorImatges img').attr('src');
-    $('#imatgesProjectes .contenidorImatges li a').mouseover(function() {
-        $('#imatgesProjectes .imatgeGran img').attr('src', $(this).find('img').attr('src'));
-    });
-    $('#imatgesProjectes a').mouseout(function() {
-        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePetitaAnterior);
-    });
+//    $('#imatgesProjectes .contenidorImatges li a').mouseover(function() {
+//        $('#imatgesProjectes .imatgeGran img').attr('src', $(this).find('img').attr('src'));
+//    });
+//    $('#imatgesProjectes a').mouseout(function() {
+//        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePetitaAnterior);
+//    });
 
     //$('#menuProjectes').tinyscrollbar({ axis: 'x' });
 
-    var imatgePreviaAnterior = $('#imatgesProjectes .contenidorImatges img').attr('src');
-    var $imatgePrevia = $('#menuProjectes .petites');
-    $imatgePrevia.find("a").mouseover(function() {
-        var nodeId = $(this).attr('data-id');
-        $('.descripcioProjecte').addClass("hide");
-        $('.contenidorImatges').addClass("hide");
-        $('#imatgesProjectes .imatgeGran img').attr('src', $("#menuProjectes .petites .hide a[data-id =" + nodeId + "]").find("img").attr('src'));
-    });
-    $('#menuProjectes .petites li').mouseleave(function() {
-        $('.descripcioProjecte').removeClass("hide");
-        $('.contenidorImatges').removeClass("hide");
-        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePreviaAnterior);
-    });
-
-    $(".fancybox-thumb").fancybox({
+//    var imatgePreviaAnterior = $('#imatgesProjectes .contenidorImatges img').attr('src');
+//    var $imatgePrevia = $('#menuProjectes .petites');
+//    $imatgePrevia.find("a").mouseover(function() {
+//        var nodeId = $(this).attr('data-id');
+//        $('.descripcioProjecte').addClass("hide");
+//        $('.contenidorImatges').addClass("hide");
+//        $('#imatgesProjectes .imatgeGran img').attr('src', $("#menuProjectes .petites .hide a[data-id =" + nodeId + "]").find("img").attr('src'));
+//    });
+//    $('#menuProjectes .petites li').mouseleave(function() {
+//        $('.descripcioProjecte').removeClass("hide");
+//        $('.contenidorImatges').removeClass("hide");
+//        $('#imatgesProjectes .imatgeGran img').attr('src', imatgePreviaAnterior);
+//    });
+    jQuery("#nivoslider-15").nivoSlider({
+        effect: "fade",
+            slices: 15,
+            //boxCols: 8,
+            //boxRows: 4,
+            animSpeed: 500,
+            pauseTime: 3000,
+            //startSlide: 0,
+            directionNav: true,
+            controlNav: true,
+            controlNavThumbs: false,
+            pauseOnHover: true,
+            manualAdvance: false
+        });
+    $(".fancybox").fancybox({
         prevEffect: 'none',
         nextEffect: 'none',
         helpers: {
